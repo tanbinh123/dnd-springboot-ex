f87edbf77b45d97e8446b79d0f74e868bd1eab16
thenewseason <thenewseason@daum.net>
2021-04-09 05:11:43 +0900

    Posts update, get api 추가

--- src/main/java/com/dnd/example/springboot/service/PostsService.java
+++ src/main/java/com/dnd/example/springboot/service/PostsService.java
@@ -1,7 +1,10 @@
 package com.dnd.example.springboot.service;
 
+import com.dnd.example.springboot.domain.post.Posts;
 import com.dnd.example.springboot.domain.post.PostsRepository;
+import com.dnd.example.springboot.web.dto.PostsResponseDto;
 import com.dnd.example.springboot.web.dto.PostsSaveRequestDto;
+import com.dnd.example.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,5 +19,26 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        // JPA 의 엔티티 매니저가 활성화된 상태로(Spring Data Jpa 에서는 default true)
+        // 트랜잭션 안에서 데이터베이스에서 데이터를 가져오면
+        // 영속성 컨텍스트가 유지된 상태임
+        // 이 상태에서 해당 데이터의 값을 변경하면 트랜잭션이 끝나는 시점에 해당 테이블에 반영.
+        // 이 개념을 더티 체킹 이라고 함.
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- src/test/java/com/dnd/example/springboot/web/PostApiControllerTest.java
+++ src/test/java/com/dnd/example/springboot/web/PostApiControllerTest.java
@@ -3,6 +3,7 @@ package com.dnd.example.springboot.web;
 import com.dnd.example.springboot.domain.post.Posts;
 import com.dnd.example.springboot.domain.post.PostsRepository;
 import com.dnd.example.springboot.web.dto.PostsSaveRequestDto;
+import com.dnd.example.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,8 +20,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -39,7 +40,7 @@ public class PostApiControllerTest {
     }
 
     @Test
-    public void Posts_register() throws Exception {
+    public void posts_register() throws Exception {
         // given
         String title = "title";
         String content = "content";
@@ -62,5 +63,39 @@ public class PostApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void posts_update() throws Exception {
+        // given
+        Posts saved = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        String title = "updated title";
+        String content = "updated content";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(title)
+                .content(content)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + saved.getId();
+        HttpEntity<PostsUpdateRequestDto> httpEntity = new HttpEntity<>(requestDto);
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, httpEntity, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        Posts updated = postsRepository.findById(saved.getId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + saved.getId()));
+
+        assertThat(updated.getTitle()).isEqualTo(title);
+        assertThat(updated.getContent()).isEqualTo(content);
     }
 }
