d77d60476594f33db6a7cfb766fbff1c570b6cee
thenewseason <thenewseason@daum.net>
2021-04-11 08:24:14 +0900

    mustache 사용 설정 및 샘플

---
+++ src/main/java/com/dnd/example/springboot/web/IndexController.java
@@ -0,0 +1,19 @@
+package com.dnd.example.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        // mustache starter 가
+        // controller 에서 문자열을 반환하면
+        // 앞의 경로와 뒤의 확장자를 자동으로 지정한다.
+        // 앞 : src/main/resources/templates
+        // 뒤 : .mustache
+        // 따라서 src/main/resources/templates/index.mustache 로 전환되어 view resolver 가 처리
+        return "index";
+    }
+}
