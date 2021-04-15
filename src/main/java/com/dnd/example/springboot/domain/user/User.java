dc8e1af34730cc4349da24b05592feaa10ff1423
thenewseason <thenewseason@daum.net>
2021-04-16 05:41:19 +0900

    User, Role entity, repository 정의

---
+++ src/main/java/com/dnd/example/springboot/domain/user/User.java
@@ -0,0 +1,51 @@
+package com.dnd.example.springboot.domain.user;
+
+import com.dnd.example.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    // JPA 로 DB에 저장할 때 Enum 값을 어떤 형태로 저장할지 결정
+    // 기본적으로 int 로 저장되는데, String 으로 지정하면 문자열로 저장됨
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
